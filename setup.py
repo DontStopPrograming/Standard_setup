0990698b405d962eb0036a54daf07624d8e5cfe1
DontStopPrograming <01dyamond01@gmail.com>
2024-07-17 22:06:39 -0500

    Feat: standard setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#Colors
+
+endCol = ''
+
+greenCol = '\e[0;32m\033[1m'
+redCol = '\e[0;31m\033[1m'
+blueCol = '\e[0;34m\033[1m'
+yellowCol = '\e[0;33m\033[1m'
+purpleCol = '\e[0;35m\033[1m'
+turquoiseCol = '\e[0;36m\033[1m'
+grayCol = '\e[0;37m\033[1m'
